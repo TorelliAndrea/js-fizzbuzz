cd3f9106c3e5ec56cdbabcbcc610690b90048f9b
Andrea Torelli <gioran326@protonmail.com>
2023-02-24 23:36:02 +0100

    tentativo conteggio punti

--- js/main.js
+++ js/main.js
@@ -2,13 +2,16 @@ for(let i = 1; i <= 100; i++) {
 
    const griglia = document.getElementById("griglia");
    const div = document.createElement("div");
-
+   let x = 0; //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+   document.getElementById("counter").innerHTML = x;
+   
    if (i % 15 == 0) {
       div.append(i);
       div.addEventListener('click', 
          function () {
             div.innerHTML ="FizzBuzz";
             div.className = "fizzbuzz";
+            x = x + 5; //perche non si somma a x?
          }
       )   
    } else if (i % 5 == 0) {
@@ -17,6 +20,7 @@ for(let i = 1; i <= 100; i++) {
          function () {
             div.innerHTML= "Buzz";
             div.className = "buzz";
+            x = x + 3; //perche non si somma a x?
          }
       )      
    } else if (i % 3 == 0) {
@@ -25,6 +29,7 @@ for(let i = 1; i <= 100; i++) {
          function () {
             div.innerHTML = "Fizz";
             div.className = "fizz";
+            x = x + 2; //perche non si somma a x?
          }
       )      
    } else {
