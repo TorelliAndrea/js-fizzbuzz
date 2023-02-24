36db6a22537a8e6e80626bde6a76846dd4b9ca3e
Andrea Torelli <gioran326@protonmail.com>
2023-02-24 22:57:14 +0100

    FizzBuzz gioco funzionante!

--- js/main.js
+++ js/main.js
@@ -4,14 +4,29 @@ for(let i = 1; i <= 100; i++) {
    const div = document.createElement("div");
 
    if (i % 15 == 0) {
-      div.append("FizzBuzz");
-      div.className = "fizzbuzz";
+      div.append(i);
+      div.addEventListener('click', 
+         function () {
+            div.innerHTML ="FizzBuzz";
+            div.className = "fizzbuzz";
+         }
+      )   
    } else if (i % 5 == 0) {
-      div.append("Buzz");
-      div.className = "buzz";
+      div.append(i);
+      div.addEventListener('click', 
+         function () {
+            div.innerHTML= "Buzz";
+            div.className = "buzz";
+         }
+      )      
    } else if (i % 3 == 0) {
-      div.append("Fizz");
-      div.className = "fizz";
+      div.append(i);
+      div.addEventListener('click', 
+         function () {
+            div.innerHTML = "Fizz";
+            div.className = "fizz";
+         }
+      )      
    } else {
       div.append(i);
       div.className = "numeri";
